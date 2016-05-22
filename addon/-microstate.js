fd9c3d3786c8bca4aa66701ccaf163cea9316a7f
Charles Lowell <cowboyd@frontside.io>
2016-05-22 14:32:27 -0700

    add support for events

---
+++ addon/-microstate.js
@@ -0,0 +1,47 @@
+import Ember from 'ember';
+
+export default Ember.Helper.extend({
+  compute([value = this.default], options) {
+    this.options = options;
+    if (!this.value || !this._update) {
+      this.value = value;
+    }
+    let current = this.value;
+    let actions = Object.keys(this.actions).reduce((actions, key)=> {
+      actions[key] = {
+        value: (...args)=> {
+          let action = this.actions[key];
+          var nextState = this.setState((curr) => action.call(null, curr, ...args));
+          sendActionNotification(this, key, nextState);
+          return nextState;
+        }
+      };
+      return actions;
+    }, {});
+
+    return Object.create(this.wrap(current), actions);
+  },
+
+  wrap(value) {
+    return value;
+  },
+
+  setState(updateFn = (current)=> current) {
+    this._update = true;
+    var nextState = updateFn.call(this, this.value);
+    if (nextState !== this.value) {
+      this.value = nextState;
+      this.recompute();
+      sendActionNotification(this, 'state', nextState);
+    }
+    return this.value;
+  }
+});
+
+function sendActionNotification(helper, actionName, state) {
+  var actionCallback = helper.options[Ember.String.dasherize(`on-${actionName}`)];
+  Ember.sendEvent(helper, actionName, state);
+  if (actionCallback && actionCallback.call) {
+    actionCallback.call(null, state);
+  }
+}
---
+++ tests/unit/helpers/boolean-test.js
@@ -0,0 +1,58 @@
+/* jshint expr:true */
+import Ember from 'ember';
+import { expect } from 'chai';
+import { describe, beforeEach, it } from 'mocha';
+import sinon from 'sinon';
+import BooleanHelper from 'ember-microstates/helpers/boolean';
+
+describe('Boolean', function() {
+  let onState = null;
+  let onToggle = null;
+  let onStateEvent = null;
+  let onToggleEvent = null;
+  let onRecompute = null;
+  beforeEach(function() {
+    [onState, onToggle, onStateEvent, onToggleEvent, onRecompute] = [
+      sinon.spy(), sinon.spy(), sinon.spy(), sinon.spy(), sinon.spy()
+    ];
+    this.helper = BooleanHelper.create({
+      recompute: onRecompute
+    });
+
+    Ember.on(this.helper, 'state', onStateEvent);
+    Ember.on(this.helper, 'toggle', onToggleEvent);
+    this.value = this.helper.compute([true], {'on-state': onState, 'on-toggle': onToggle});
+  });
+
+  it("computes to the initial state ", function() {
+    expect(this.value.value).to.equal(true);
+  });
+
+  describe("toggling", function() {
+    beforeEach(function() {
+      this.toggled = this.value.toggle();
+    });
+
+    it("swaps the state to false", function() {
+      expect(this.toggled).to.equal(false);
+    });
+
+    it.skip("fires the 'state' event", function() {
+      expect(onStateEvent.called).to.equal(true);
+    });
+
+    it("invokes the on-state callback", function() {
+      expect(onState.calledWith(false)).to.equal(true);
+    });
+
+    it.skip("fires the 'toggle' event", function() {
+      expect(onStateEvent.called).to.equal(true);
+    });
+
+    it("invokes the on-toggle callback", function() {
+      expect(onToggle.calledWith(false)).to.equal(true);
+    });
+  });
+
+
+});
