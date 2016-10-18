06ea01f52c0d671d120070d5d2f7933e3c9c730c
Taras Mankovski <tarasm@gmail.com>
2016-10-18 12:29:38 -0400

    Added comment to helpers/object#handlebarsValueFor

--- addon/helpers/object.js
+++ addon/helpers/object.js
@@ -8,6 +8,13 @@ export default MicroState.extend({
     return assign({}, value);
   },
 
+  /**
+   * Make all of the object properties explicitly enumerable in the template
+   * 
+   * Handlebars helpers like `each-in` use `Object.keys` to iterate object properties. `Object.keys` returns
+   * only properties must be owned by the object. This hook will assign each value's property onto the object,
+   * to ensure that it can be retrieved with `Object.keys`.
+   */
   handlebarsValueFor(object, value) {
     return reduceObject(value, function(result, name, value) {
       return assign(result, {
