07470799b612c3dd8db9c2472d2dfe4ab0caf324
Taras Mankovski <tarasm@gmail.com>
2018-06-18 18:25:18 -0500

    Bumped dependencies and eliminated use of UMD

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-microstates',
-
-  included(app) {
-    this._super.included.apply(this, arguments);
-  
-    app.import('node_modules/microstates/dist/microstates.umd.js', {
-      using: [
-        { transformation: 'amd', as: 'microstates' }
-      ]
-    });
-  }
+  name: 'ember-microstates'
 };
