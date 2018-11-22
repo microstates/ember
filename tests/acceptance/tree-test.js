ef937867adc7f2dfe973c8dc2e35a22e28930172
Taras Mankovski <tarasm@gmail.com>
2018-11-22 06:27:36 -0500

    Starting to add more tests

---
+++ tests/acceptance/tree-test.js
@@ -0,0 +1,55 @@
+/* jshint expr:true */
+import {
+  describe,
+  it,
+  beforeEach,
+  afterEach
+} from 'mocha';
+import { expect } from 'chai';
+import startApp from '../helpers/start-app';
+import destroyApp from '../helpers/destroy-app';
+import $ from 'jquery';
+
+describe('Acceptance: Boolean', function() {
+  let application;
+
+  beforeEach(function() {
+    application = startApp();
+  });
+
+  afterEach(function() {
+    destroyApp(application);
+  });
+
+  beforeEach(function() {
+    return visit('/');
+  });
+
+  it("shows the initial boolean state", function() {
+    expect($('.spec-boolean-value').text().trim()).to.equal('false');
+  });
+  describe("hitting the toggle button", function() {
+    beforeEach(function() {
+      click('.spec-toggle');
+    });
+    it("changes the boolean state to true", function() {
+      expect($('.spec-boolean-value').text().trim()).to.equal('true');
+    });
+  });
+  describe("hitting the set true button", function() {
+    beforeEach(function() {
+      click('.spec-set-true');
+    });
+    it("changes the boolean state to true", function() {
+      expect($('.spec-boolean-value').text().trim()).to.equal('true');
+    });
+  });
+  describe("hitting the set false button", function() {
+    beforeEach(function() {
+      click('.spec-set-false');
+    });
+    it("has no effect", function() {
+      expect($('.spec-boolean-value').text().trim()).to.equal('false');
+    });
+  });
+});
--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -4,5 +4,40 @@ export default Controller.extend({
   groceries: ["Milk", "Cereal", "Donuts"],
   animals: ["Horse", "Cow", "Lizard"],
   initialCar: { color: "red", model: "Mustang", make: "Ford" },
-  additional: { speed: "fast", suspension: "stiff"}
+  additional: { speed: "fast", suspension: "stiff" },
+  tree: {
+    children: [
+      {
+        children: [
+          {
+            children: [
+              {
+                children: [
+                  {
+                    children: [
+                      {
+                        children: [
+                          { children: [{}], isOpen: true, counter: 3 }
+                        ],
+                        isOpen: true
+                      }
+                    ],
+                    isOpen: true,
+                    counter: 1
+                  }
+                ],
+                isOpen: true,
+                counter: 1
+              }
+            ],
+            isOpen: true,
+            counter: 1
+          }
+        ],
+        isOpen: true,
+        counter: 1
+      }
+    ],
+    isOpen: true
+  }
 });
