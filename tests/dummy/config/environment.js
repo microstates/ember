fe3db26fb8b93b2caffcec43cbaf8948f30551e3
Taras Mankovski <tarasm@gmail.com>
2018-11-26 20:45:06 -0500

    Made tests pass

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -20,6 +20,15 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    fontawesome: {
+      icons: {
+        'free-solid-svg-icons': 'all',
+        'free-brands-svg-icons': [
+          'github'
+        ]
+      }
     }
   };
 
