3693d16be5907692696236b750514da5d9be8c9a
Ilya Radchenko <knownasilya@gmail.com>
2019-08-23 13:11:29 -0400

    fix: some tests

--- tests/integration/helpers/value-of-test.js
+++ tests/integration/helpers/value-of-test.js
@@ -2,14 +2,14 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupRenderingTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
-import { render, click } from '@ember/test-helpers';
+import { render, click, find } from '@ember/test-helpers';
 
 describe('Integration | Helper | value-of', function() {
   setupRenderingTest();
 
   it('extracts value from the microstate', async function() {
 
-    render(hbs`
+    await render(hbs`
       {{#let (state 42) as |$|}}
         {{on-render (action (mut value)) (value-of $)}}
         <button {{action $.increment}} />
